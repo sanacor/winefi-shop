895465c1a39a9c1f64f47dde3d53e62040fb117e
sanha kang <ncsana@gmail.com>
2021-12-14 21:58:59 +0900

    MaterialTable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import MaterialTableDemo from './demo'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '30px' }}>
+      <h1>Wine-Fi</h1>
+      <MaterialTableDemo />
     </div>
   );
 }
-
 export default App;
---
+++ src/demo.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import MaterialTable from 'material-table';
+import axios from "axios";
+
+
+export default function MaterialTableDemo() {
+  // const [state, setState] = React.useState({
+    // columns: [
+    //   { title: 'Name', field: 'name' },
+    //   { title: 'Surname', field: 'surname' },
+    //   { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+    //   {
+    //     title: 'Birth Place',
+    //     field: 'birthCity',
+    //     lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+    //   },
+    // ],
+    // data: [
+    //   { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+    //   {
+    //     name: 'Zerya Betül',
+    //     surname: 'Baran',
+    //     birthYear: 2017,
+    //     birthCity: 34,
+    //   },
+    // ],
+  // });
+  const [state, setState] = React.useState({ hits: [] });
+  React.useEffect(async () => {
+    const result = await axios.get(
+      "http://demo3440516.mockable.io/inquiry"
+    );
+    setState(result.data);
+  }, []);
+
+  return (
+    <MaterialTable
+      title="문의 리스트"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: newData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data.push(newData);
+              setState({ ...state, data });
+            }, 600);
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data[data.indexOf(oldData)] = newData;
+              setState({ ...state, data });
+            }, 600);
+          }),
+        onRowDelete: oldData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data.splice(data.indexOf(oldData), 1);
+              setState({ ...state, data });
+            }, 600);
+          }),
+      }}
+    />
+  );
+}
