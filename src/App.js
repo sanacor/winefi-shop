6520bc44b44c989a93c829c6219341c71360677b
ooni0921 <ooni0921@gmail.com>
2021-12-21 22:51:51 +0900

    react router 적용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import './App.css';
-import MaterialTableDemo from './demo'
-import SignIn from './signIn'
+import SignIn from './containers/SignIn';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Home from './containers/Home';
+import NotFound from './containers/NotFound';
+import Profile from './containers/Profile';
 
 
 function App() {
+  const [user, setUser] = useState(null);
+  const authenticated = user != null;
+
+  const login = ({ email, password }) => setUser({ "ID" : "user", "PWD" : "TEST" });
+
   return (
     <div style={{ padding: '30px' }}>
-      <h1>Wine-Fi</h1>
-      <SignIn />
+    <BrowserRouter>
+      <header>
+        <h1>Wine-Fi 가맹점 페이지</h1>
+        <Link to="/">
+          <button>Home</button>
+        </Link>
+        <Link to="/profile">
+          <button>Profile</button>
+        </Link>
+      </header>
+      <hr />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/login"
+            element={<SignIn />}
+          />
+          <Route
+            path="/profile"
+            element={<Profile />}
+          />
+          <Route path="/*" component={NotFound} />
+        </Routes>
+      </main>
+    </BrowserRouter>
     </div>
+
+    
   );
 }
 export default App;
