d364a669f62a0d1561dbeeea0ca3124d8eefcad5
sanha kang <ncsana@gmail.com>
2022-01-10 12:59:39 +0900

    React Route 기본 구조

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+
 import MaterialTableDemo from './demo'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import About from './pages/About';
+import Home from './pages/Home';
 
 
 function App() {
   return (
-    <div style={{ padding: '30px' }}>
-      <h1>Wine-Fi</h1>
-      <MaterialTableDemo />
+    // <div style={{ padding: '30px' }}>
+    //   <h1>Wine-Fi</h1>
+    //   <MaterialTableDemo />
+    // </div>
+    <div>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
     </div>
   );
 }
