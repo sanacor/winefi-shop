0ea6b6e71e1575c050b67a204d78d5b1a5ec09fd
sanha kang <ncsana@gmail.com>
2022-01-26 23:05:11 +0900

    hi

--- src/components/profile.component.js
+++ src/components/profile.component.js
@@ -3,27 +3,120 @@ import { Redirect } from 'react-router-dom';
 import { useSelector } from "react-redux";
 import parseJwt from "../helpers/JWTParser";
 
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal'
+import MaterialTable from "material-table";
+import axios from "axios";
+
+
+
 function Profile(props) {
   const { user } = useSelector((state) => state.loginSlice );
   const [info1, info2] = parseJwt(user.access_token);
   console.log(info1, info2);
 
+
+  ///
+  const [show, setShow] = React.useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+ 
+  // modal end
+
+  const [state, setState] = React.useState({ hits: [] });
+  React.useEffect(async () => {
+    const result = await axios.get("http://demo3440516.mockable.io/inquiry");
+    setState(result.data);
+  }, []);
+  ///
   if (!user) {
     return <Redirect to="/login" />;
   }
   
   return (
-    <div className="container">
-      <p>
-          <strong>Access Token:</strong> {user.access_token}
-          <br/>
-          {JSON.stringify(info1)}
-          <br/>
-          {JSON.stringify(info2)}
-          <br/>
-          <strong>Refresh Token:</strong> {user.refresh_token} 
-        </p>
-    </div>
+    <>
+      <MaterialTable
+        title="문의 리스트"
+        columns={state.columns}
+        data={state.data}
+        actions={[
+          {
+            icon: "search",
+            tooltip: "문의 자세히 보기",
+            onClick: (event, rowData) => {
+              // Do save operation
+              console.log("onClick: 문의 자세히 보기");
+              handleShow();
+            },
+          },
+          {
+            icon: "edit",
+            tooltip: "답변 하기",
+            onClick: (event, rowData) => {
+              // Do save operation
+              alert("Hi 답변 하기");
+            },
+          },
+        ]}
+        localization={{
+          header: {
+            actions: "기능",
+          },
+        }}
+        // editable={{
+        //   // isEditable: rowData => rowData.name === 'replyContents', // only name(a) rows would be editable
+        //   onRowAdd: (newData) =>
+        //     new Promise((resolve) => {
+        //       setTimeout(() => {
+        //         resolve();
+        //         const data = [...state.data];
+        //         data.push(newData);
+        //         setState({ ...state, data });
+        //       }, 600);
+        //     }),
+        //   onRowUpdate: (newData, oldData) =>
+        //     new Promise((resolve) => {
+        //       setTimeout(() => {
+        //         resolve();
+        //         const data = [...state.data];
+        //         data[data.indexOf(oldData)] = newData;
+        //         setState({ ...state, data });
+        //       }, 600);
+        //     }),
+        //   // onRowDelete: oldData =>
+        //   //   new Promise(resolve => {
+        //   //     setTimeout(() => {
+        //   //       resolve();
+        //   //       const data = [...state.data];
+        //   //       data.splice(data.indexOf(oldData), 1);
+        //   //       setState({ ...state, data });
+        //   //     }, 600);
+        //   //   }),
+        // }}
+      />
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          I will not close if you click outside me. Don't even try to press
+          escape key.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">Understood</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
   /*
   return (
