0cffe0c1fe3eee7b46fee47afba90bff28ceff76
DESKTOP-HVRJ26N\ooni0 <ooni0921@gmail.com>
2022-02-02 20:30:05 +0900

    Service Workder를 8버전대로 수정(9버전대 왜 안되냐..)

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,10 +1,13 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging/sw";
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
-const firebaseApp = initializeApp({
+firebase.initializeApp({
     apiKey: 'AIzaSyDLiZs17HJ2eOQWfiiwuyCXIHW3qJTaGb8',
     authDomain: 'winecode-e8f6f.firebaseapp.com',
     //databaseURL: 'https://project-id.firebaseio.com',
@@ -17,4 +20,27 @@ const firebaseApp = initializeApp({
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-const messaging = getMessaging(firebaseApp);
+const messaging = firebase.messaging();
+
+
+
+//import { initializeApp } from "firebase/app";
+//import { getMessaging } from "firebase/messaging/sw";
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+// const firebaseApp = initializeApp({
+//     apiKey: 'AIzaSyDLiZs17HJ2eOQWfiiwuyCXIHW3qJTaGb8',
+//     authDomain: 'winecode-e8f6f.firebaseapp.com',
+//     //databaseURL: 'https://project-id.firebaseio.com',
+//     projectId: 'winecode-e8f6f',
+//     storageBucket: 'winecode-e8f6f.appspot.com',
+//     messagingSenderId: '441215028866',
+//     appId: '1:441215028866:web:2ae7755c70a3abfb60ae9b',
+//     measurementId: 'G-CB87P1432X',
+// });
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+// const messaging = getMessaging(firebaseApp);
